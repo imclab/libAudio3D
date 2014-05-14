bb4e1a8674419144de5c8e560b4fda04adee3844
Julius Kammerl <julius@kammerl.de>
2014-05-14 12:06:30 -0700

    hrtf data structure wrapper

---
+++ include/hrtf_data.h
@@ -0,0 +1,42 @@
+#ifndef HRTF_DATA_H_
+#define HRTF_DATA_H_
+
+#include <string>
+
+#ifdef MIT_KEMAR
+#include "mit_kemar_hrtf_data.h"
+#endif
+
+struct HRTFDataT {
+  // Data set identifier
+  const std::string identifier;
+
+  // Number of HRTF sample points in polar coordinates
+  const int num_hrtfs;
+
+  // Length of HRTF FIRs
+  const int fir_length;
+
+  const int sample_rate;
+
+  // Orientations as {elevation, azimuth} pairs.
+  const int (*direction)[2];
+
+  // Distance of sound source in meters.
+  const float distance;
+
+  // HRTF data
+  const short (*data)[2][kHRTFFilterLen];
+};
+
+static const HRTFDataT kHRTFDataSet = {
+    kHRTFDataIdentifier,
+    kHRTFNum,
+    kHRTFFilterLen,
+    kHRTFSampleRate,
+    kHRTFDirection,
+    kDistanceMeter,
+    kHRTFData
+};
+
+#endif
