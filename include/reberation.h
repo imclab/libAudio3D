eecd9cbce32a24b5d91edf4999d4b76dc3146cd3
Julius Kammerl <julius@kammerl.de>
2014-05-14 12:04:46 -0700

    adding reberation class (simple exponential decay))

---
+++ include/reberation.h
@@ -0,0 +1,36 @@
+#ifndef REBERATION_H_
+#define REBERATION_H_
+
+#include <vector>
+class FFTFilter;
+class Reberation {
+public:
+	Reberation(int block_size, int sampling_rate, float reberation_time);
+	float GetQuietPeriod() const;
+
+	void AddReberation(const std::vector<float>& input,
+			std::vector<float>* output_left, std::vector<float>* output_right);
+
+	const std::vector<float>& GetImpulseResponseLeft() const;
+	const std::vector<float>& GetImpulseResponseRight() const;
+
+private:
+	void RenderImpulseResponse(int block_size,
+			int sampling_rate, float reberation_time);
+	static float FloatRand();
+	int block_size_;
+	std::vector<float> impulse_response_left_;
+	std::vector<float> impulse_response_right_;
+	float quiet_period_sec_;
+
+	FFTFilter* left_reberation_filter_;
+	FFTFilter* right_reberation_filter_;
+	std::vector<float> reberation_input_;
+	std::vector<float> reberation_output_left_;
+	std::vector<float> reberation_output_right_;
+	int reberation_output_read_pos_;
+
+};
+
+#endif
+
