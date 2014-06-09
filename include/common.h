0525d792d0950902c4e72ed36dbce2d4ac7d5d41
Julius Kammerl <julius@kammerl.de>
2014-06-08 21:55:50 -0700

    common header and impl

--- include/common.h
+++ include/common.h
@@ -1,9 +1,16 @@
 #ifndef COMMON_H_
 #define COMMON_H_
 
+#include <cmath>
+#include <complex>
+#include <vector>
+#include <assert.h>
+
 static const float kSoundSpeedMeterPerSecond = 340.0;
 
 static const int kDimensions = 3;
+
+typedef std::complex<float> Complex;
 
 union Point3D {
   float data[kDimensions];
@@ -14,6 +21,14 @@ union Point3D {
   };
 };
 
+Point3D operator+(const Point3D& point_a, const Point3D& point_b) ;
+
+Point3D operator-(const Point3D& point_a, const Point3D& point_b);
+
+float PointDot(const Point3D& point_a, const Point3D& point_b);
+
+float VecLen(const Point3D& point_a);
+
 struct AcousticSource {
   Point3D pos;
   float damping;
@@ -21,7 +36,18 @@ struct AcousticSource {
   bool do_render;
 };
 
+struct HRTFInfo {
+  HRTFInfo();
+  int distance_index;
+  int hrtf_index;
+  bool left_right_swap;
+};
+
+bool operator==(const HRTFInfo& hrtf_a, const HRTFInfo& hrtf_b);
+bool operator!=(const HRTFInfo& hrtf_a, const HRTFInfo& hrtf_b);
+
 union Plane {
+  float norm[3];  // plane normal
   float data[4];  // plane equation
   struct {
     float a;
---
+++ src/common.cpp
@@ -0,0 +1,44 @@
+#include "common.h"
+#include <assert.h>
+
+Point3D operator+(const Point3D& point_a, const Point3D& point_b) {
+  Point3D ret;
+  ret.x = point_a.x + point_b.x;
+  ret.y = point_a.y + point_b.y;
+  ret.z = point_a.z + point_b.z;
+  return ret;
+}
+
+Point3D operator-(const Point3D& point_a, const Point3D& point_b) {
+  Point3D ret;
+  ret.x = point_a.x - point_b.x;
+  ret.y = point_a.y - point_b.y;
+  ret.z = point_a.z - point_b.z;
+  return ret;
+}
+
+float PointDot(const Point3D& point_a, const Point3D& point_b) {
+  return point_a.x * point_b.x + point_a.y * point_b.y + point_a.z * point_b.z;
+}
+
+float VecLen(const Point3D& point_a) {
+  return sqrt(PointDot(point_a, point_a));
+}
+
+
+HRTFInfo:: HRTFInfo()
+: distance_index(-1),
+  hrtf_index(-1),
+  left_right_swap(false) {
+}
+
+bool operator==(const HRTFInfo& hrtf_a, const HRTFInfo& hrtf_b) {
+  return hrtf_a.distance_index == hrtf_b.distance_index
+      && hrtf_a.hrtf_index == hrtf_b.hrtf_index
+      && hrtf_a.left_right_swap == hrtf_b.left_right_swap;
+}
+
+bool operator!=(const HRTFInfo& hrtf_a, const HRTFInfo& hrtf_b) {
+  return !operator==(hrtf_a, hrtf_b);
+}
+
